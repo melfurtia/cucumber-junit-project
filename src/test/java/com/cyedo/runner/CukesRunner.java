3c50a826aa524d95545c9997399f993a0a3d44cf
Melfu <melfurtia@gmail.com>
2022-11-07 16:41:57 -0500

    Cuckes updated

--- src/test/java/com/cyedo/runner/CukesRunner.java
+++ src/test/java/com/cyedo/runner/CukesRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber.json",
+        plugin = {
+                "json:target/cucumber.json",
                 "pretty",
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt" ,
